b63f1d2085661a1fc13cc1fd93323e03b41afcd6
Woroud <woroudfouleh26@gmail.com>
2024-11-16 18:46:04 +0300

    Initial commit

---
+++ src/main/java/com/mycompany/woroud/User.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.woroud;
+
+/**
+ *
+ * @author Woroud Fouleh
+ */
+public class User {
+      public static  String CurrentUser;
+      public static String getCurrentUser()
+      {
+          return CurrentUser;
+      }
+      public static void setCurrentUser(String CurrenUser)
+      {
+          CurrentUser=CurrenUser;
+      }
+  public static   String CurrentUserPassword;
+  public static String getCurrentUserPssword()
+      {
+          return CurrentUserPassword;
+      }
+      public static void setCurrentUserPassword(String CurrentUserpas)
+      {
+          CurrentUserPassword=CurrentUserpas;
+      }
+  public static String CurrentFname;
+  public static String getCurrentFname()
+      {
+          return CurrentFname;
+      }
+      public static void setCurrentFname(String CurrentFnam)
+      {
+          CurrentFname=CurrentFnam;
+      }
+    public static   String CurrentLname;
+    public static String getCurrentLname()
+      {
+          return CurrentLname;
+      }
+      public static void setCurrentLname(String CurrentLnam)
+      {
+          CurrentLname=CurrentLnam;
+      }
+      public static   String Currentgender;
+      public static String getCurrentgender()
+      {
+          return Currentgender;
+      }
+      public static void setCurrentgender(String Currentgende)
+      {
+          Currentgender=Currentgende;
+      }
+  public static   int OrderCounter;
+  public static int getOrderCounter()
+      {
+          return OrderCounter;
+      }
+      public static void setOrderCounter(int CurrentOrderCount)
+      {
+          OrderCounter=CurrentOrderCount;
+      }
+  public static   String Currentcity;
+  public static String getCurrentCity()
+      {
+          return Currentcity;
+      }
+      public static void setCurrentCity(String Currentcit)
+      {
+          Currentcity=Currentcit;
+      }
+public static   String Currentphone;
+public static String getCurrentPhone()
+      {
+          return Currentphone;
+      }
+      public static void setCurrentPhine(String Currentphon)
+      {
+          Currentphone=Currentphon;
+      }
+}
